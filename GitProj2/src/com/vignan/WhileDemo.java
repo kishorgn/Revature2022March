5aebb51c960116de2fcba210bb88cdc736423ce1
kishor <kishor@Kishor-ThinkPad>
2022-03-14 19:06:30 +0530

    Commit - 3 : Proj2 with whiledemo

---
+++ GitProj2/src/com/vignan/WhileDemo.java
@@ -0,0 +1,16 @@
+package com.vignan;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to while");
+		int c = 1;
+		while(c < 20) {
+			c += c ;
+			System.out.println(c);
+		}
+		System.out.println("Thank you!!!");
+
+	}
+
+}
