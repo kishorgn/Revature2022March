f24f33a90f51388a186668659bbff4c5a4fee422
kishor <kishor@Kishor-ThinkPad>
2022-03-14 19:04:26 +0530

    Commit - 2 : Proj1 with fordemo

---
+++ GitProj1/src/com/revature/app/ForDemo.java
@@ -0,0 +1,14 @@
+package com.revature.app;
+
+public class ForDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to for loop");
+		for(int c = 0 ; c < 10 ; c++) {
+			System.out.println(c);
+		}
+		System.out.println("Thank you!!!");
+
+	}
+
+}
