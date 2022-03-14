28a0c4b5f027e5528ee70802a29f28145658961c
kishor <kishor@Kishor-ThinkPad>
2022-03-14 19:11:49 +0530

    Commit - 5 : After switchbranch, demo-1

---
+++ GitProj1/src/com/revature/app/Switch_01.java
@@ -0,0 +1,11 @@
+package com.revature.app;
+
+public class Switch_01 {
+
+	public static void main(String[] args) {
+		System.out.println("This is swith demo 01");
+		System.out.println("This is working good.");
+
+	}
+
+}
