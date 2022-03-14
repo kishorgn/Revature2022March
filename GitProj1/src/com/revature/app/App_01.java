1c0cde55051a5412c68d49630c8ab9436029abd6
kishor <kishor@Kishor-ThinkPad>
2022-03-14 19:02:42 +0530

    Commit - 1 : Both the projects with single program.

---
+++ GitProj1/src/com/revature/app/App_01.java
@@ -0,0 +1,10 @@
+package com.revature.app;
+
+public class App_01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!!!");
+
+	}
+
+}
---
+++ GitProj2/src/com/vignan/Application_01.java
@@ -0,0 +1,10 @@
+package com.vignan;
+
+public class Application_01 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Vignan's Lara");
+
+	}
+
+}
